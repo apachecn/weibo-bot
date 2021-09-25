11ee10d4368f81544a6ab7797d3b7710ee866a97
wizardforcel <562826179@qq.com>
2021-09-25 22:26:04 +0800

    2021-09-25 22:26:04

--- main.py
+++ main.py
@@ -2,19 +2,21 @@
 # author: wizardforcel
 
 import requests
-from bs4 import BeautifulSoup
+from pyquery import PyQuery as pq
 import datetime as dt
 import pytz
 import random
 import time
 import json
 import urllib
+import os
+
+# KEY = os.environ.get('WB_KEY')
+# SEC = os.environ.get('WB_SEC')
+COOKIE = os.environ.get('WB_COOKIE', '')
 
 with open('repos.json') as f:
     repos = json.loads(f.read())
-
-with open('cookie') as f:
-    cookie = f.read().strip()
 
 hdrs = {
     "Pragma": "no-cache",
@@ -26,68 +28,83 @@ hdrs = {
     "Accept": "*/*",
     "Referer": "https://weibo.com/2216356441/manage?default=9527",
     "Accept-Language": "zh-CN",
-    "Cookie": cookie,
+    "Cookie": COOKIE,
 }
 
-def get_title(html):
-    rt = BeautifulSoup(html, 'lxml')
-    h1 = rt.select('article > h1')
-    if len(h1) == 0:
-        return ''
-    return h1[0].text
+def parse_cookie(cookie):
+    res = {}
+    for kv in cookie.split('; '):
+        kv = kv.split('=')
+        if len(kv) < 2: continue
+        res[kv[0]] = kv[1]
+    return res
 
-def get_summary_link(html):
-    rt = BeautifulSoup(html, 'lxml')
-    links = rt.select('td.content a')
-    for link in links:
-        if link.text == 'SUMMARY.md':
-            return 'https://github.com' + \
-                link.attrs['href']
-    return ''
+def refresh_cookie():
+    old = parse_cookie(hdrs['Cookie'])
+    r = requests.get('https://weibo.com/', headers=hdrs)
+    new = parse_cookie(r.headers.get('Set-Cookie', ''))
+    old.update(new)
+    hdrs['Cookie'] = '; '.join([
+        f'{k}={v}' for k, v in old.items()
+    ])
+    
+
+def get_title(html):
+    rt = pq(html)
+    h1 = rt.find('.markdown-body > h1').eq(0)
+    return h1.text().strip()
 
 def get_random_ch(html):
-    rt = BeautifulSoup(html, 'lxml')
-    links = rt.select('article li a')[1:]
-    link = random.choice(links)
-    return 'https://github.com' + link.attrs['href']
+    rt = pq(html)
+    el_links = rt.find('.markdown-body li a')
+    links = [
+        'https://gitee.com' +
+        el_links.eq(i).attr('href')
+        for i in range(len(el_links))
+    ]
+    return random.choice(links)
 
-n_post = 20
-tz = pytz.timezone('PRC')
-now = dt.datetime.now(tz)
+def main():
+    refresh_cookie()
+    print('Cookie: ' + hdrs.get('Cookie', ''))
+    n_post = 20
+    tz = pytz.timezone('PRC')
+    now = dt.datetime.now(tz)
+
+    for _ in range(n_post):
+        hr = random.randint(0, 23)
+        min = random.randint(0, 59)
+        dt_ = now + dt.timedelta(hours=hr, minutes=min)
+        dt_str = dt_.strftime('%Y-%m-%d %H:%M')
+        
+        repo = random.choice(repos)
+        url = f'https://gitee.com/{repo}'
+        co = requests.get(url).text
+        title = get_title(co)
+        summary = f'{url}/blob/master/SUMMARY.md'
+        co = requests.get(summary).text
+        link = get_random_ch(co)
+        co = requests.get(link).text
+        sub_title = get_title(co)
+        text = f'『{title}』{sub_title}（来源：Gitee/@ApacheCN ） {link}' \
+            .replace('https://', '')
+        print(text)
+
+        post_str = f'location=page_100505_manage&text={urllib.parse.quote(text)}&style_type=1&pdetail=1005052216356441&isReEdit=false&rank=0&addtime={dt_str}&pub_type=dialog&_t=0'
+        ts = int(time.time() * 1000)
+        url = f'https://weibo.com/aj/mblog/add?ajwvr=6&__rnd={ts}'
+        res = requests.post(url, data=post_str, headers=hdrs).content.decode('gbk')
+        
+        try:
+            j = json.loads(res)
+        except json.decoder.JSONDecodeError as ex:
+            print(f'{dt_str} 发送失败：尚未登录')
+            continue
+        
+        if j['code'] == '100000':
+            print(f'{dt_str} 发送成功！')
+        else:
+            msg = j['msg']
+            print(f'{dt_str} 发送失败：{msg}')
     
-for _ in range(n_post):
-    hr = random.randint(0, 23)
-    min = random.randint(0, 59)
-    dt_ = now + dt.timedelta(hours=hr, minutes=min)
-    dt_str = dt_.strftime('%Y-%m-%d %H:%M')
-    
-    repo = random.choice(repos)
-    co = requests.get(repo).text
-    title = get_title(co)
-    summary = get_summary_link(co)
-    if summary == '': continue
-    co = requests.get(summary).text
-    link = get_random_ch(co)
-    co = requests.get(link).text
-    sub_title = get_title(co)
-    print(title + ' ' + sub_title)
-    text = f'『{title}』{sub_title}（来源：Github/@ApacheCN ） {link}'
-    
-    post_str = f'location=page_100505_manage&text={urllib.parse.quote(text)}&style_type=1&pdetail=1005052216356441&isReEdit=false&rank=0&addtime={dt_str}&pub_type=dialog&_t=0'
-    ts = int(time.time() * 1000)
-    url = f'https://weibo.com/aj/mblog/add?ajwvr=6&__rnd={ts}'
-    res = requests.post(url, data=post_str, headers=hdrs).content.decode('gbk')
-    
-    try:
-        j = json.loads(res)
-    except json.decoder.JSONDecodeError as ex:
-        print(f'{dt_str} 发送失败：尚未登录')
-        continue
-    
-    if j['code'] == '100000':
-        print(f'{dt_str} 发送成功！')
-    else:
-        msg = j['msg']
-        print(f'{dt_str} 发送失败：{msg}')
-    
-    
+if __name__ == '__main__': main()
